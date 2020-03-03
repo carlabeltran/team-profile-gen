4c22c4de1fffc7d42ae1d1ab61cb05504904db36
carlabeltran <carlabeltran14@icloud.com>
2020-03-03 02:03:58 -0600

    updated test files &lib files, created team html page

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ const inquirer = require("inquirer");
 const path = require("path");
 
 //
-const Manager = require("./lib/Manager");
+const Manager = require("./lib/manager");
 
 //
-const Engineer = require("./lib/Engineer");
+const Engineer = require("./lib/engineer");
 
 //
-const Intern = require("./lib/Intern");
+const Intern = require("./lib/intern");
 
 //
 const inquirer = require("inquirer");
@@ -26,9 +26,18 @@ const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 //
-const render = require("./lib/htmlRenderer");
+const render = require("./lib/htmlRender");
+
 
 const employees = [];
 
+function addEmployee() {
+    inquirer.prompt([{
 
+        
+
+
+
+    }])
+}
 
